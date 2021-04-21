e37490499691471131147104927f89127758a635
c332030 <c332030@gmail.com>
2021-04-21 20:45:47 +0800

    feat: del mirror.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,18 @@
 set -e
 
 sh -c "/init-git-ssh.sh $*"
-sh -c "/mirror.sh $*"
+
+SOURCE_REPO=$1
+DESTINATION_REPO=$2
+
+echo "SOURCE=$SOURCE_REPO"
+echo "DESTINATION=$DESTINATION_REPO"
+
+git clone --mirror "$SOURCE_REPO" && cd "$(basename "$SOURCE_REPO")"
+git remote set-url --push origin "$DESTINATION_REPO"
+
+git fetch -p origin
+
+# Exclude refs created by GitHub for pull request.
+git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+git push --mirror
